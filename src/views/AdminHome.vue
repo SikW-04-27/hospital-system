2c31d23777bd6fcf0656dca7ad13622eeba96a6e
盛炜 张 <1374281404@qq.com>
2024-05-03 22:52:07 +0800

    feat: 更新管理端的菜单icon

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+import {
+  OfficeBuilding,
+  Calendar,
+  Refrigerator,
+  User,
+  Histogram,
+  Wallet
+} from '@element-plus/icons-vue'
 import { RouterView } from 'vue-router'
 </script>
 <template>
@@ -10,27 +17,27 @@ import { RouterView } from 'vue-router'
         <el-scrollbar>
           <el-menu default-active="hospitalList" style="height: 100vh">
             <el-menu-item index="hospitalList">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><OfficeBuilding /></el-icon>
               <span>医院管理</span>
             </el-menu-item>
             <el-menu-item index="2">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><Refrigerator /></el-icon>
               <span>科室管理</span>
             </el-menu-item>
             <el-menu-item index="3">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><Calendar /></el-icon>
               <span>排班管理</span>
             </el-menu-item>
             <el-menu-item index="4">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><User /></el-icon>
               <span>用户管理</span>
             </el-menu-item>
             <el-menu-item index="5">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><Wallet /></el-icon>
               <span>订单管理</span>
             </el-menu-item>
             <el-menu-item index="6">
-              <el-icon><icon-menu /></el-icon>
+              <el-icon><Histogram /></el-icon>
               <span>统计管理</span>
             </el-menu-item>
           </el-menu>
